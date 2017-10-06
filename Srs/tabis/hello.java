566a9113fb2c0d98ba3db4c314e2ddaf6ea2d2bb
Karakat <karakat1406@gmail.com>
2017-10-06 13:30:13 +0600

    Ekinwi kommit

--- Srs/tabis/hello.java
+++ Srs/tabis/hello.java
@@ -3,6 +3,8 @@ package tabis;
 public class hello {
     public static void main(String[] args){
         System.out.println("Hello World!");
+        System.out.println("Byl pthxanalik jumis");
+        System.out.println("GitHub-pen jumis");
 
     }
 }
